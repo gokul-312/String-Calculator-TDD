d05b9de90fe2bc7c2daed020ab1ef6018bcabcfc
Gokul312 <gokulakrishnanr2000@gmail.com>
2025-03-17 15:46:49 +0530

    Basic requirement done 2. handle unknown amount of numbers 3. new line delimiter to split numbers 4. support dynamic delimiter 5. raise in negative values

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,5 +1,31 @@
+# class: string calculator
+# methods: @add (string) -> returns (integer)
+
 class StringCalculater
+
   def add(string)
     return 0 if string.empty?
+
+    values = find_values string
+    raise_if_negative values
+    values.sum
+  end
+
+  def find_values(string)
+    if string.start_with?('//')
+      delimiter, numbers = string.split("\n", 2)
+      delimiter = delimiter[2..]
+      numbers.split(delimiter)
+    else
+      string.split(/,|\n/)
+    end
+  end
+
+  def raise_if_negative(values)
+    values.map!(&:to_i)
+    negatives = values.select(&:negative?)
+    return if negatives.empty?
+
+    raise "Negatives not allowed: #{negatives.join(', ')}"
   end
 end
--- string_calculator_test.rb
+++ string_calculator_test.rb
@@ -1,5 +1,7 @@
-require 'minitest/autorun'
-require './string_calculator.rb'
+# test for class: StringCalculator
+
+require "minitest/autorun"
+require "./string_calculator.rb"
 
 class StringCalculatorTest < Minitest::Spec
   before do
@@ -7,6 +9,58 @@ class StringCalculatorTest < Minitest::Spec
   end
 
   def test_empty_string
-    assert_equal 0, @string_calc.add('')
+    assert_equal 0, @string_calc.add("")
+  end
+
+  def test_split_comma
+    assert_equal 5, @string_calc.add("2,3")
+  end
+
+  def test_split_comma_more_value
+    assert_equal 10, @string_calc.add("1,2,3,4")
+  end
+
+  def test_split_lines
+    assert_equal 6, @string_calc.add("1\n2,3")
+  end
+
+  def test_dynamic_delimiter
+    assert_equal 3, @string_calc.add("//;\n1;2")
+  end
+
+  def test_exception_for_negative
+    assert_raises RuntimeError, "Negatives not allowed: -1" do
+      @string_calc.add("-1,2")
+    end
+  end
+
+  ## find_values
+  def test_find_values_with_comma
+    assert_equal %w[1 2 3], @string_calc.find_values("1,2,3")
+  end
+
+  def test_find_values_with_newline
+    assert_equal %w[1 2 3], @string_calc.find_values("1\n2,3")
+  end
+
+  def test_find_values_with_custom_delimiter
+    assert_equal %w[1 2], @string_calc.find_values("//;\n1;2")
+  end
+
+  ## raise_if_negative
+  def test_raise_if_negative_with_no_negatives
+    assert_nil @string_calc.raise_if_negative([1, 2, 3])
+  end
+
+  def test_raise_if_negative_with_negatives
+    assert_raises RuntimeError, "Negatives not allowed: -1, -2" do
+      @string_calc.raise_if_negative([-1, 2, -2])
+    end
+  end
+
+  def test_raise_if_negative_with_all_negatives
+    assert_raises RuntimeError, "Negatives not allowed: -1, -2, -3" do
+      @string_calc.raise_if_negative([-1, -2, -3])
+    end
   end
 end
